7ca2394c71e23964ecf59583a0e634383faf6bce
Mihai Voinea <mihaivoinea01@gmail.com>
2019-05-02 19:57:12 +0300

    added super simple interface; no css yet :3

--- functions/webhook.js
+++ functions/webhook.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
 
 exports.handler = (event, context, callback) => {
+  if (event.httpMethod !== 'POST') {
+    callback(null, {
+      statusCode: 200,
+      body: 'Hello people! :3 \nSay hello back on Twitter @mihaivoinea_ \n\nBTW, you\'re not supposted to be here.\nGo back and watch the video tutorial!',
+    });
+  }
   const secret = event.path.split('/')[4];
   const username = event.path.split('/')[5];
   if (secret !== null && username !== null) {
