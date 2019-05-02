bdc75cbb1b372c347b69d51a9ed398a73d09ac01
Mihai Voinea <mihaivoinea01@gmail.com>
2019-05-02 20:10:51 +0300

    remove #makersdrink

--- functions/webhook.js
+++ functions/webhook.js
@@ -11,7 +11,7 @@ exports.handler = (event, context, callback) => {
   const username = event.path.split('/')[5];
   if (secret !== null && username !== null) {
     const body = JSON.parse(event.body).response;
-    const message = `${body.supporter_name || 'Somebody'} just send me ☕️ ${body.number_of_coffees} Coffees on BuyMeACoffee.com/${username} #makersdrink`;
+    const message = `${body.supporter_name || 'Somebody'} just send me ☕️ ${body.number_of_coffees} Coffees on BuyMeACoffee.com/${username}`;
 
     axios.post(`https://api.getmakerlog.com/apps/webhook/${secret}`, {
       content: message,
